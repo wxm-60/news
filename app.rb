e79a23dae8d9fc496f2d28ddaab0fe751e9c845b
wxm-60 <sharon.wang2@kellogg.northwestern.edu>
2020-05-18 21:18:39 +0000

    add news and weather api calls

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ get "/" do
   long = -87.6722787
 
   units = "imperial" # or metric, whatever you like
-  key = "YOUR-API-KEY-GOES-HERE" # replace this with your real OpenWeather API key
+  key = "be5864316f7aa735ea6bcc2c03bcb750" # replace this with your real OpenWeather API key
 
   # construct the URL to get the API data (https://openweathermap.org/api/one-call-api)
   url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{long}&units=#{units}&appid=#{key}"
@@ -18,5 +18,22 @@ get "/" do
   # make the call
   @forecast = HTTParty.get(url).parsed_response.to_hash
 
+  @current_weather = "#{@forecast["current"]["temp"]} degrees and #{@forecast["current"]["weather"][0]["description"]}"
+  @extended_forecast = []
+  i = 0
+  for day in @forecast["daily"]
+    @extended_forecast << ["#{i}", "#{day["weather"][0]["main"]} with a high of #{day["temp"]["max"]} and a low of #{day["temp"]["min"]}"]
+    i = i + 1
+  end 
+
   ### Get the news
+  url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=f663fbf56ba5479488d3d40b0fc58755"
+  @news = HTTParty.get(url).parsed_response.to_hash
+
+  @topnews=[]
+  for article in @news["articles"]
+    @topnews << ["#{article["title"]}","#{article["url"]}"]
+  end 
+
+  view "news"  
 end
